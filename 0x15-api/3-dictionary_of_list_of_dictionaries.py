b652b5b37ae42379a087b37c647bf6c214a03f3d
astianmuchui <astianmuchui@gmail.com>
2024-01-08 11:28:53 +0300

    Export to JSON

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Gather data for all employees
+"""
+
+if __name__ == "__main__":
+    import json
+    import requests
+    import sys
+
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+    todo = requests.get(url + "todos").json()
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({user.get("id"): [{
+            "task": task.get("title"),
+            "completed": task.get("completed"),
+            "username": user.get("username")
+        } for task in todo if user.get("id") == task.get("userId")]
+        for user in users}, jsonfile)
