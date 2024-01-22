489212a98129b00257a08e4dfa03cd1a6b68de48
astianmuchui <astianmuchui@gmail.com>
2024-01-22 03:45:58 +0300

    Add comment on first line

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,13 @@
 # Check why apache returns a 500 error and fix it
+
 file { '/var/www/html/wp-settings.php':
   ensure => present,
   owner  => 'www-data',
   group  => 'www-data',
   mode   => '0644',
+}
+
+exec { 'fix_apache':
+  command => 'sed -i s/php/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
